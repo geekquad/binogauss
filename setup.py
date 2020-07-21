cf980c2b845eadd3cbbca3dc98697107f27bef56
geekquad <adityaastranaut@gmail.com>
2020-07-22 02:28:34 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binogauss',
+      version='0.1',
+      description='Gaussian and Binomial distribution',
+      packages=['binogauss'],
+      author='geekquad', 
+      author_email = 'adityaastranaut@gmail.com',
+      zip_safe=False)
